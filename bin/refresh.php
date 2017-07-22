143d01e789039ba115bede8a1027f13f2927ca96
Daisuke Inoue <hibariyashiki@gmail.com>
2017-07-22 13:23:12 +0900

    use if in require.

--- bin/refresh.php
+++ bin/refresh.php
@@ -1,11 +1,16 @@
 <?php
-	require '../Sinsituwoka.php';
-
+	//	require '../Sinsituwoka.php';
+	if (file_exists('../vendor/autoload.php')) {
+		require '../vendor/autoload.php';
+	} else {
+		require '../../vendor/autoload.php';
+	}
 	use Sinsituwoka\Sinsituwoka;
+	//use GuzzleHttp\Client;
 
 	$s = new Sinsituwoka();
 
 	print_r( $s->accessTokenFromLocal());
-
+	
 	var_dump( $s->refresh());
 	var_dump( $s->bearer());

9f1c65d68fa850026f564e55739d7b35f56dcbec
Daisuke Inoue <hibariyashiki@gmail.com>
2017-07-22 13:31:51 +0900

    del unnec lines.

--- bin/refresh.php
+++ bin/refresh.php
@@ -1,16 +1,13 @@
 <?php
-	//	require '../Sinsituwoka.php';
 	if (file_exists('../vendor/autoload.php')) {
 		require '../vendor/autoload.php';
 	} else {
 		require '../../../autoload.php';
 	}
 	use Sinsituwoka\Sinsituwoka;
-	//use GuzzleHttp\Client;
 
 	$s = new Sinsituwoka();
 
 	print_r( $s->accessTokenFromLocal());
 	var_dump( $s->refresh());
 	var_dump( $s->bearer());
-	echo __DIR__;
